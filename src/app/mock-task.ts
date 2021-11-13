6fbe9dd52cc0b0e5cf881fdbdd7c5bcaefcdea2e
nour144 <bounasrnour@gmail.com>
2021-11-13 20:36:53 +0200

    first commit

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import {Task} from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text:'Doctors appoitement',
+        day:'day here',
+        reminder: true,
+    },
+    {
+        id:2,
+        text:'task2',
+        day:'day here',
+        reminder: true,
+    },
+    {
+        id:3,
+        text:'task 3',
+        day:'day here',
+        reminder: false,
+    },
+    {
+        id:4,
+        text:'task 4',
+        day:'day here',
+        reminder: true,
+    }
+]
